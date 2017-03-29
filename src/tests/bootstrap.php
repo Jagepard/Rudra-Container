cb512e33014a55ff365e8791f97fe73f332f2e0b
Jagepard <dankorot@gmail.com>
2017-03-29 19:44:15 +0300

    Добавляет необязательный strict_types

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
-/**
- * Date: 17.02.17
- * Time: 14:25
- *
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2016, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
- */
+declare(strict_types = 1);
 
 require_once dirname(__DIR__) . '/ContainerTrait.php';
 require_once dirname(__DIR__) . '/IContainer.php';
