4ee50b1cf26a43f89e54f29779d74cb34ad185dc
Jagepard <dankorot@gmail.com>
2017-04-06 15:21:30 +0300

    Добавляет ContainerGlobalsTrait

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 require_once dirname(__DIR__) . '/ContainerSessionTrait.php';
-require_once dirname(__DIR__) . '/ContainerFilesTrait.php';
+require_once dirname(__DIR__) . '/ContainerGlobalsTrait.php';
 require_once dirname(__DIR__) . '/ContainerCookieTrait.php';
 require_once dirname(__DIR__) . '/ContainerTrait.php';
 require_once dirname(__DIR__) . '/IContainer.php';
