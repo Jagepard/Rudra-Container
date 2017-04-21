71e1841bc9e0c0cda9ded3fc816be0d9ef41f5e5
Jagepard <dankorot@gmail.com>
2017-04-21 11:50:52 +0300

    Добавляет SetContainerTrait

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+require_once dirname(__DIR__) . '/SetContainerTrait.php';
 require_once dirname(__DIR__) . '/ContainerSessionTrait.php';
 require_once dirname(__DIR__) . '/ContainerGlobalsTrait.php';
 require_once dirname(__DIR__) . '/ContainerCookieTrait.php';
