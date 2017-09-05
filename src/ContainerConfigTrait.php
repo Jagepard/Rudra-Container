37eadf61b7203d87ed85cabb66b56ddc445fb658
Jagepard <dankorot@gmail.com>
2017-09-05 17:45:47 +0300

    Добавляет ContainerConfigTrait

---
+++ src/ContainerConfigTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2017, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Rudra;
+
+/**
+ * Class ContainerConfigTrait
+ *
+ * @package Rudra
+ */
+trait ContainerConfigTrait
+{
+
+    /**
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * @param string      $key
+     * @param string|null $subKey
+     *
+     * @return array|mixed
+     */
+    public function config(string $key, string $subKey = null)
+    {
+        return ($subKey === null) ? $this->config[$key] : $this->config[$key][$subKey];
+    }
+
+    /**
+     * @param array $config
+     */
+    public function setConfig(array $config)
+    {
+        $this->config = $config;
+    }
+}
