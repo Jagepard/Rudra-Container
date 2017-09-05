73db56e3abfde8a84ca9bf14d109ce7139e6eeda
Jagepard <dankorot@gmail.com>
2017-09-05 17:45:17 +0300

    Обновляет

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -2,13 +2,7 @@
 
 declare(strict_types = 1);
 
-require_once dirname(__DIR__) . '/SetContainerTrait.php';
-require_once dirname(__DIR__) . '/ContainerSessionTrait.php';
-require_once dirname(__DIR__) . '/ContainerGlobalsTrait.php';
-require_once dirname(__DIR__) . '/ContainerCookieTrait.php';
-require_once dirname(__DIR__) . '/ContainerTrait.php';
-require_once dirname(__DIR__) . '/ContainerInterface.php';
-require_once dirname(__DIR__) . '/Container.php';
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
 require_once __DIR__ . '/stub/ClassWithContainerTrait.php';
 require_once __DIR__ . '/stub/ClassWithoutConstructor.php';
