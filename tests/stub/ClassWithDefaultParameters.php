c6ed2c62ce57db64bacade9808ce39ed132bad44
Jagepard <dankorot@gmail.com>
2018-05-30 13:10:52 +0300

    Update

--- tests/stub/ClassWithDefaultParameters.php
+++ tests/stub/ClassWithDefaultParameters.php
@@ -5,7 +5,18 @@ namespace Rudra\Tests\Stub;
 class ClassWithDefaultParameters
 {
 
+    protected $param;
+
     public function __construct(string $param = 'Default')
     {
+        $this->param = $param;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParam(): string
+    {
+        return $this->param;
     }
 }
