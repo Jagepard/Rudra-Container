4dceb30ef2a3672e03f084bbe5157a7fa70d30d8
Jagepard <dankorot@gmail.com>
2020-09-02 17:56:48 +0300

    update

--- src/Session.php
+++ src/Session.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 /**
  * @author    : Jagepard <jagepard@yandex.ru">
- * @copyright Copyright (c) 2019, Jagepard
  * @license   https://mit-license.org/ MIT
  */
 
@@ -14,10 +13,6 @@ use Rudra\Container\Interfaces\ContainerInterface;
 
 class Session implements ContainerInterface
 {
-    /**
-     * @param  string|null  $key
-     * @return mixed
-     */
     public function get(string $key = null)
     {
         if (empty($key)) {
@@ -25,22 +20,19 @@ class Session implements ContainerInterface
         }
 
         if (!array_key_exists($key, $_SESSION)) {
-            throw new \InvalidArgumentException('no data corresponding to the key');
+            throw new \InvalidArgumentException("No data corresponding to the key");
         }
 
         return $_SESSION[$key];
     }
 
-    /**
-     * @param  array  $data
-     */
     public function set(array $data): void
     {
         if (count($data) !== 2) {
-            throw new \InvalidArgumentException('the array contains the wrong number of elements');
+            throw new \InvalidArgumentException("The array contains the wrong number of elements");
         }
 
-        if (is_array($data[1]) && (key($data[1]) === 'increment')) {
+        if (is_array($data[1]) && (key($data[1]) === "increment")) {
             $_SESSION[$data[0]][] = $data[1];
             return;
         }
@@ -48,18 +40,11 @@ class Session implements ContainerInterface
         $_SESSION[$data[0]] = $data[1];
     }
 
-    /**
-     * @param  string  $key
-     * @return bool
-     */
     public function has(string $key): bool
     {
         return isset($_SESSION[$key]);
     }
 
-    /**
-     * @param  string  $key
-     */
     public function unset(string $key): void
     {
         unset($_SESSION[$key]);
