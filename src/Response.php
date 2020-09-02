733410984f3d67a4ff105d943ba29f7c127242ec
Jagepard <dankorot@gmail.com>
2020-09-02 17:55:02 +0300

    update

--- src/Response.php
+++ src/Response.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 /**
  * @author    : Jagepard <jagepard@yandex.ru">
- * @copyright Copyright (c) 2019, Jagepard
  * @license   https://mit-license.org/ MIT
  */
 
@@ -16,18 +15,13 @@ class Response implements ResponseInterface
 {
     /**
      * @codeCoverageIgnore
-     * @param array $data
      */
     public function json(array $data): void
     {
-        header('Content-Type: application/json');
+        header("Content-Type: application/json");
         print $this->getJson($data);
     }
 
-    /**
-     * @param array $data
-     * @return string
-     */
     private function getJson(array $data): string
     {
         return json_encode($data);
