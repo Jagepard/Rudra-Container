4139bb43ae022df14745172dcca5657cb4665adf
Jagepard <dankorot@gmail.com>
2020-09-24 17:57:56 +0300

    rollback to ContainerInterface

--- src/Session.php
+++ src/Session.php
@@ -9,9 +9,9 @@ declare(strict_types=1);
 
 namespace Rudra\Container;
 
-use Rudra\Container\Abstracts\AbstractContainer;
+use Rudra\Container\Abstracts\ContainerInterface;
 
-class Session extends AbstractContainer
+class Session implements ContainerInterface
 {
     public function get(string $key = null)
     {
