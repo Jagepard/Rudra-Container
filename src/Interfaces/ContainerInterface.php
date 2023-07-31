eba33622145b277c95771734dcf89648c96a6ec1
Jagepard <dankorot@gmail.com>
2023-07-31 16:29:21 +0300

    updates comments

--- src/Interfaces/ContainerInterface.php
+++ src/Interfaces/ContainerInterface.php
@@ -10,32 +10,32 @@ namespace Rudra\Container\Interfaces;
 interface ContainerInterface
 {
     /**
-     * @param  string|null $key
-     * @return void
-     * 
      * Gets the container element
      * --------------------------
      * Получает элемент контейнера
+     * 
+     * @param  string|null $key
+     * @return void
      */
     public function get(string $key = null);
 
     /**
-     * @param  array $data
-     * @return void
-     * 
      * Adds data to the container
      * --------------------------
      * Добавляет данные в контейнер
+     * 
+     * @param  array $data
+     * @return void
      */
     public function set(array $data): void;
 
     /**
-     * @param  string  $key
-     * @return boolean
-     * 
      * Checks if the element is in the container
      * -----------------------------------------
      * Проверяет наличие элемента в контейнере
+     * 
+     * @param  string  $key
+     * @return boolean
      */
     public function has(string $key): bool;
 }
--- src/Interfaces/RudraInterface.php
+++ src/Interfaces/RudraInterface.php
@@ -12,18 +12,78 @@ use Rudra\Container\Session;
 
 interface RudraInterface
 {
-    // Config, Services and Binding
+    /**
+     * Creates a configuration container
+     * ---------------------------------
+     * Создает контейнер конфигураций
+     * 
+     * @param  array              $config
+     * @return ContainerInterface
+     */
     public function config(array $config = []): ContainerInterface;
+
+    /**
+     * Creates a container with a list of services
+     * -------------------------------------------
+     * Создает контейнер со списком серверов
+     * 
+     * @param  array              $services
+     * @return ContainerInterface
+     */
     public function serviceList(array $services = []): ContainerInterface;
+
+    /**
+     * Creates a container to associate interfaces with implementations
+     * ----------------------------------------------------------------
+     * Создает контейнер для связи интерфейсов с реализациями
+     * 
+     * @param  array              $contracts
+     * @return ContainerInterface
+     */
     public function binding(array $contracts = []): ContainerInterface;
 
-    // Containers for the HTTP / 1.1 Common Method Kit
+    /**
+     * Containers for the HTTP / 1.1 Common Method Kit
+     * -----------------------------------------------
+     * Контейнеры для HTTP/1.1 Common Method Kit
+     * 
+     * @return RequestInterface
+     */
     public function request(): RequestInterface;
-    // For different types of responses
+
+    /**
+     * Initializes the service for different types of responses
+     * ------------------------------------------------
+     * Инициализирует сервис для разных типов ответов
+     * 
+     * @return ResponseInterface
+     */
     public function response(): ResponseInterface;
-    // Creates the main singleton
+
+    /**
+     * Creates the main application singleton
+     * --------------------------------------
+     * Создает основной синглтон приложения
+     * 
+     * @return RudraInterface
+     */
     public static function run(): RudraInterface;
 
+    /**
+     * Initializes the cookie service
+     * -------------------------------------------
+     * Инициализирует сервис для работы с cookie
+     * 
+     * @return Cookie
+     */
     public function cookie(): Cookie;
+
+    /**
+     * Initializes the service for working with sessions
+     * -------------------------------------------------
+     * Инициализирует сервис для работы с сессиями
+     * 
+     * @return Session
+     */
     public function session(): Session;
 }
