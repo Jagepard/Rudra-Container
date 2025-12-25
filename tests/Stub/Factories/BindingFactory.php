4c38ad9b4de9e5ce64b658fd51f205a784f4e1ce
Jagepard <jagepard@yandex.ru>
2025-12-25 18:10:07 +0300

    change license

--- tests/Stub/Factories/BindingFactory.php
+++ tests/Stub/Factories/BindingFactory.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @author  : Jagepard <jagepard@yandex.ru">
- * @license https://mit-license.org/ MIT
- */
-
 namespace Rudra\Container\Tests\Stub\Factories;
 
 use Rudra\Container\Tests\Stub\BindingClass;
--- tests/Stub/Interfaces/BindInterface.php
+++ tests/Stub/Interfaces/BindInterface.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @author  : Jagepard <jagepard@yandex.ru">
- * @license https://mit-license.org/ MIT
- */
-
  namespace Rudra\Container\Tests\Stub\Interfaces;
 
  interface BindInterface{}
