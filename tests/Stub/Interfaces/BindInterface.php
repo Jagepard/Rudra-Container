b88cb84215a6a274482f0cdea1834fb58bfbd179
Jagepard <jagepard@yandex.ru>
2025-12-26 20:25:33 +0300

    fix

--- tests/Stub/Interfaces/BindInterface.php
+++ tests/Stub/Interfaces/BindInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @author  Korotkov Danila (Jagepard) <jagepard@yandex.ru>
+ * @license https://mozilla.org/MPL/2.0/  MPL-2.0
+ */
+
  namespace Rudra\Container\Tests\Stub\Interfaces;
 
  interface BindInterface{}
